81599efa9785d4c4788e71f283bda8acb2b9d2a5
Paul Joey Clark <joeytwiddle@gmail.com>
2020-04-02 17:00:58 +0800

    Update git-alaises.sh

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,10 +1,14 @@
+# See also: shell-aliases.sh
+
 alias gs='git status'
-alias gl='git log'
-alias gll='git log --oneline --graph'
-alias glp='git log --patch'
+alias gl='git log --oneline --graph'
+alias gll='git log --stat --patch'
 
 alias gd='git diff'
+# Use this to stage changes
 alias gap='git add --patch'
+# Use this to stage new files
+alias ga='git add'
 # This shows what you have staged so far
 alias gdc='git diff --cached'
 alias gc='git commit'
@@ -20,6 +24,8 @@ alias gmom='git merge origin/master'
 alias gmod='git merge origin/develop'
 alias grom='git rebase --autostash origin/master'
 alias grod='git rebase --autostash origin/develop'
+alias gmu='git merge @{upstream}'
+alias gru='git rebase --autostash @{upstream}'
 
 alias gri='git rebase --interactive'
 alias grc='git rebase --continue'
@@ -28,9 +34,10 @@ alias gcp='git cherry-pick'
 
 alias gss='git stash save'
 alias gsp='git stash pop'
-# Only stash those lines you say 'y' to (remove them for now, pop them back later)
+# For stashing away some unwanted changes, but keeping the rest
+# Only stashes those lines you say 'y' to (remove them for now, pop them back later)
 alias gssp='git stash --patch'
 # Remove ("kill") whatever lines you say 'y' to (stashes what you select, then drops the stash)
 alias gssk='git stash --patch && git stash drop'
-# (If you accidentally kill something you wanted, recovery is possible using
-# `git reflog`, which means this is safer than `git reset --hard`)
+# This is safer than `git reset --hard` because if you did accidentally kill
+# something you wanted, recovery is possible from `git reflog`
